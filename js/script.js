bedc6f4e685aa6bba1015bee0039666fbd2e32da
anandukch <anandukch@gmail.com>
2021-11-18 19:33:10 +0530

    mute button added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var splash = document.querySelector(".splash-screen");
 var video = document.getElementById("video");
+var muteButton=document.querySelector(".mute-button");
+var circle=document.querySelector(".button-circle");
 video.pause();
 document.addEventListener("DOMContentLoaded", async () => {
   setTimeout(() => {
@@ -10,7 +12,10 @@ document.addEventListener("DOMContentLoaded", async () => {
     video.play();
   }, 4000);
 });
-function c() {
+
+muteButton.addEventListener("click", toggleMute);
+function toggleMute() {
   if (video.muted) video.muted = false;
   else video.muted = true;
+  muteButton.classList.toggle("mute-button-active");
 }
