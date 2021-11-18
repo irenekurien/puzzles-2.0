e5739d7eb148abfb802d6078a8eabc15074811d5
anandukch <anandukch@gmail.com>
2021-11-18 21:13:39 +0530

    mute button fixed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 var splash = document.querySelector(".splash-screen");
 var video = document.getElementById("video");
 var muteButton=document.querySelector(".mute-button");
-var circle=document.querySelector(".button-circle");
 video.pause();
 document.addEventListener("DOMContentLoaded", async () => {
   setTimeout(() => {
-    splash.classList.add("remove-splash");
+    if(video.readyState>=3){
+      splash.classList.add("remove-splash");
     setTimeout(() => {
       splash.remove();
     }, 2000);
     video.play();
+    } 
   }, 4000);
 });
 
 muteButton.addEventListener("click", toggleMute);
 function toggleMute() {
+  muteButton.innerHTML = video.muted ? "<i class='fas fa-volume-up'></i>" : "<i class='fas fa-volume-mute'></i>";
   if (video.muted) video.muted = false;
   else video.muted = true;
-  muteButton.classList.toggle("mute-button-active");
 }
