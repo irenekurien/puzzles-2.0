be456ce90fcc5fdea38426e541cd71bb493c5fed
anandukch <anandukch@gmail.com>
2021-11-20 22:43:59 +0530

    updatd var with const

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 const splash = document.querySelector(".splash-screen");
 const video = document.getElementById("video");
-const muteButton=document.querySelector(".mute-button");
+const muteButton = document.querySelector(".mute-button");
 video.pause();
 document.addEventListener("DOMContentLoaded", async () => {
   setTimeout(() => {
-    if(video.readyState>=3){
+    if (video.readyState >= 3) {
       splash.classList.add("remove-splash");
-    setTimeout(() => {
-      splash.remove();
-    }, 2000);
-    video.play();
-    } 
+      setTimeout(() => {
+        splash.remove();
+      }, 2000);
+      video.play();
+    }
   }, 4000);
 });
 
 muteButton.addEventListener("click", toggleMute);
 function toggleMute() {
-  muteButton.innerHTML = video.muted ? "<i class='fas fa-volume-up'></i>" : "<i class='fas fa-volume-mute'></i>";
+  muteButton.innerHTML = video.muted
+    ? "<i class='fas fa-volume-up'></i>"
+    : "<i class='fas fa-volume-mute'></i>";
   if (video.muted) video.muted = false;
   else video.muted = true;
 }
